5f8ba79ee40b253301ecbe8503893681f790147f
Park So Dam <parksodam0228@e-mirim.hs.kr>
2018-03-09 07:42:10 +0900

    로그인 화면 엑티비티 생성
    레이아웃 배치 필요

---
+++ app/src/main/java/kr/hs/emirim/parksodam/healingmessage/LoginActivity.java
@@ -0,0 +1,13 @@
+package kr.hs.emirim.parksodam.healingmessage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
