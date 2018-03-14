df20741784458b25c142ab038658b94e36b24b8e
Unknown <aza923@e-mirim.hs.kr>
2018-03-15 00:35:47 +0900

    LoginActivity 기분이 좋아지는 메세지 자동으로 랜덤 출력

---
+++ app/src/main/java/kr/hs/emirim/parksodam/healingmessage/TextChangeThread.java
@@ -0,0 +1,35 @@
+package kr.hs.emirim.parksodam.healingmessage;
+
+import android.util.Log;
+import android.widget.TextView;
+
+/**
+ * Created by 민경 on 2018-03-14.
+ */
+public class TextChangeThread extends Thread {
+    private static final String TAG = "ExampleThread";
+    String[] word = {"삶이 있는 한 희망은 있어 -키케로", "잘했어", "오늘 하루도 수고했어", "힘내!", "오늘은 내일을 위한 첫걸음",
+            "진정으로 웃으려면 고통을 참아야하며, 나아가 고통을 즐길 줄 알아야 해 -찰리 채플린", "피할 수 없으면 즐겨라!", "내일은 내일의 태양이 뜬다",
+            "넌 말이야 네 생각보다 괜찮은 사람이야", "누가 뭐라하던 너는 너야", "무엇보다 중요한건 네 자신을 사랑하는 거야",
+            "넌 꼭 성공할거야", "지금 안된 건 더 좋은 일이 있을거라는거야", "괜찮아", "고개숙이지마, 똑바로 정면으로 바라봐", "우리는 행복하기 위해 세상에 왔어 -류시화",
+            "힘들땐 모두 울어서 떠내려보내는 것도 좋아", "내가 너의 이야기를 들어줄게", "걱정마", "너의 나무는 아직 자라는 중이야", "수고했어! 고마워!",
+            "힘내 너는 아름다우니까"};
+
+    int word_random;
+    TextView word_text;
+
+        @Override
+        public void run() {
+            for(int i=0;i<3;i++) {
+                word_random = (int) (Math.random() * 20);
+                word_text.setText(word[word_random]);
+                Log.e("dkssud", word[word_random]);
+                //word_text.setText("");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+}
