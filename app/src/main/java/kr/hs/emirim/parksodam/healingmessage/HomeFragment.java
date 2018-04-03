da9467b45847a4421fb6fac588ba7598b7759657
Unknown <josy3465@naver.com>
2018-04-04 01:42:42 +0900

    홈프래그먼트 화면 전체 비율 맞추기
    FrameLayout -> LinearLayout으로 변경 후 아래 탭바는 marginbottom으로 비워둠

--- app/src/main/java/kr/hs/emirim/parksodam/healingmessage/HomeFragment.java
+++ app/src/main/java/kr/hs/emirim/parksodam/healingmessage/HomeFragment.java
@@ -2,14 +2,25 @@ package kr.hs.emirim.parksodam.healingmessage;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import kr.hs.emirim.parksodam.healingmessage.adapter.SearchAdapter;
 import kr.hs.emirim.parksodam.healingmessage.slider.FragmentSlider;
 import kr.hs.emirim.parksodam.healingmessage.slider.SliderIndicator;
 import kr.hs.emirim.parksodam.healingmessage.slider.SliderPagerAdapter;
@@ -24,6 +35,14 @@ public class HomeFragment extends BaseFragment {
     private SliderIndicator mIndicator;
     private SliderView sliderView;
     private LinearLayout mLinearLayout;
+    private List<String> list;          // 데이터를 넣은 리스트변수
+    private ListView listView_m;          // 검색을 보여줄 리스트변수
+    private SearchAdapter adapter_m;      // 리스트뷰에 연결할 아답터
+    private ArrayList<String> arraylist;
+    private DatabaseReference databaseReference;
+    private FirebaseAuth mAuth;
+    static boolean calledAlerady = false;
+    TextView feel;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -32,6 +51,44 @@ public class HomeFragment extends BaseFragment {
         mLinearLayout = (LinearLayout) view.findViewById(R.id.pagesContainer);
 
         setupSlider();
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("users");
+        mAuth = FirebaseAuth.getInstance();
+
+        listView_m = (ListView) view.findViewById(R.id.listView_h);
+
+        // 리스트를 생성한다.
+        list = new ArrayList<String>();
+
+        // 리스트의 모든 데이터를 arraylist에 복사한다.// list 복사본을 만든다.
+        arraylist = new ArrayList<String>();
+        arraylist.addAll(list);
+
+        // 리스트에 연동될 아답터를 생성한다.
+        adapter_m = new SearchAdapter(list, getActivity());
+
+        // 리스트뷰에 아답터를 연결한다.
+        listView_m.setAdapter(adapter_m);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference databaseRef = database.getReference("users");
+
+        databaseRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for(DataSnapshot searchSnapshot : dataSnapshot.getChildren()){
+                    String str_n = searchSnapshot.child("name").getValue(String.class);
+                    Log.e("name값 불러오기", "성공?");
+                    list.add(str_n);
+                }
+                adapter_m.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.w("TAG: ", "Failed to read value", databaseError.toException());
+            }
+        });
         return view;
     }
 
@@ -49,6 +106,8 @@ public class HomeFragment extends BaseFragment {
         mIndicator.setPageCount(fragments.size());
         mIndicator.show();
     }
+
+
 
     @Override
     public String getTitle() {
---
+++ app/src/main/java/kr/hs/emirim/parksodam/healingmessage/adapter/HomeAdapter.java
@@ -0,0 +1,11 @@
+package kr.hs.emirim.parksodam.healingmessage.adapter;
+
+/**
+ * Created by JoSeoYoung on 2018-04-04.
+ */
+
+public class HomeAdapter {
+
+}
+
+
