d8371468d9163105577c79e2b152d0629e49afb3
Unknown <aza923@e-mirim.hs.kr>
2018-05-01 19:20:19 +0900

    포춘쿠키 누르면 다이얼로그 띄워서 힐링되는 말 랜덤으로 뜨게 하기

--- app/src/main/java/kr/hs/emirim/parksodam/healingmessage/myprofile/MyProfileFragment.java
+++ app/src/main/java/kr/hs/emirim/parksodam/healingmessage/myprofile/MyProfileFragment.java
@@ -8,15 +8,26 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
 
 import kr.hs.emirim.parksodam.healingmessage.BaseFragment;
 import kr.hs.emirim.parksodam.healingmessage.OnMyListener;
 import kr.hs.emirim.parksodam.healingmessage.R;
+import kr.hs.emirim.parksodam.healingmessage.adapter.HomeAdapter;
+import kr.hs.emirim.parksodam.healingmessage.search.SearchItem;
 
 import static kr.hs.emirim.parksodam.healingmessage.BarActivity.id;
 
@@ -25,10 +36,11 @@ public class MyProfileFragment extends BaseFragment {
     private View view;
     private ImageView received_message;
     private ImageView send_message;
+    //private ImageView fortune_cookie;
     private ImageView fortune_cookie;
     private DatabaseReference databaseReference;
     private FirebaseAuth mAuth;
-    private OnMyListener onMyListener;
+
     String[] word = {"삶이 있는 한 희망은 있어 -키케로", "잘했어", "오늘 하루도 수고했어", "힘내!", "오늘은 내일을 위한 첫걸음",
             "진정으로 웃으려면 고통을 참아야하며, 나아가 고통을 즐길 줄 알아야 해 -찰리 채플린", "피할 수 없으면 즐겨라!", "내일은 내일의 태양이 뜬다",
             "넌 말이야 네 생각보다 괜찮은 사람이야", "누가 뭐라하던 너는 너야", "무엇보다 중요한건 네 자신을 사랑하는 거야",
@@ -36,7 +48,8 @@ public class MyProfileFragment extends BaseFragment {
             "힘들땐 모두 울어서 떠내려보내는 것도 좋아", "내가 너의 이야기를 들어줄게", "걱정마", "너의 나무는 아직 자라는 중이야", "수고했어! 고마워!",
             "힘내 너는 아름다우니까", "불안해 한다는 건 가능성이 있기 때문에 불안한거야 -민주리선생님", "넌 정말 소중한 존재야", "네가 가고자 하는 길은 꽃길일거야",
             "꽃길만 걷게 해 줄게요 -김세정", "너는 멋진 존재야", "난 용감해, 난 당당해, 난 내가 자랑스러워 이게 나야 -this is me", "내 앞에선 다 괜찮아 울어도 돼",
-            "고마워", "사랑해", "너는 특별해"};
+            "고마워", "사랑해", "너는 특별해", "손이 타버릴 듯 뜨거울 지라도, 담고싶은 태양이 있다면 절대로 놓지 말 것", "시작하는 모든 존재는 늘 아프고 불안하다. 하지만 기억하라 그대는 눈부시게 아름답다",
+            "나는 멋지다, 나는 멋지다, 나는 멋지다 - 스피릿핑거스"};
     int word_random;
     public MyProfileFragment() {
         // Required empty public constructor
@@ -52,14 +65,13 @@ public class MyProfileFragment extends BaseFragment {
         received_message = (ImageView)  view.findViewById(R.id.received_message);
         send_message = (ImageView)  view.findViewById(R.id.send_message);
         fortune_cookie = (ImageView)  view.findViewById(R.id.fortune_cookie);
-        word_random = (int) (Math.random() * word.length);
 
         Bundle bundle = getActivity().getIntent().getExtras(); // 액티비티에서 넘어오는 값 받기
         if (bundle != null) {
             id = bundle.getString("userId");
             Log.e("tag",id);
 
-        received_message.setOnClickListener(new View.OnClickListener() { // 받은 메세지함으로 이동
+            received_message.setOnClickListener(new View.OnClickListener() { // 받은 메세지함으로 이동
             @Override
             public void onClick(View v) {
                 //onMyListener.onReceivedData(id);
@@ -76,29 +88,31 @@ public class MyProfileFragment extends BaseFragment {
             }
         });
 
-        // 포춘쿠키는 다이얼로그 띄우기
-        fortune_cookie.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(
-                        getActivity());
+            //포춘쿠키 다이얼로그 띄우기
+            fortune_cookie.setOnClickListener(new Button.OnClickListener() {
+                public void onClick(View v) {
+                    android.app.AlertDialog.Builder alert = new android.app.AlertDialog.Builder(getActivity());
 
-                // 제목셋팅
-                alertDialogBuilder.setTitle("오늘의 포춘쿠키");
+                    // 제목셋팅
+                    alert.setTitle("오늘의 포춘쿠키");
 
-                // AlertDialog 셋팅
-                alertDialogBuilder
-                        .setMessage(word[word_random]) // 좋은 말 띄우기
+                    for(int i = 0; i< word.length; i++){
+                        word_random = (int) (Math.random() * word.length);
+                    }
+                    Log.e("피치퍼펙트재밋다..", word[word_random]);
+                    alert.setMessage(word[word_random]) // 좋은 말 띄우기
                         .setCancelable(false)
                         .setPositiveButton("확인",
                                 new DialogInterface.OnClickListener() {
                                     public void onClick(
                                             DialogInterface dialog, int id) {
                                         // 포춘쿠키 확인
-                                        getActivity().finish();
+
                                     }
-                                })
-                        .setNegativeButton("닫기",
+                                });
+
+
+                    alert .setNegativeButton("닫기",
                                 new DialogInterface.OnClickListener() {
                                     public void onClick(
                                             DialogInterface dialog, int id) {
@@ -106,8 +120,11 @@ public class MyProfileFragment extends BaseFragment {
                                         dialog.cancel();
                                     }
                                 });
-            }
-        });
+
+                    alert.show();
+
+                }
+            });
 
     } else Log.e("sorry"," null");
         return view;
